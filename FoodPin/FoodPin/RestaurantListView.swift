89a6de56b5c8d658fd6ab6cef60abf7f0effadc1
sla-vak <slavak15072008@gmail.com>
2022-06-24 18:21:48 +0300

    Exercises on p 271 (FoodPin)

--- FoodPin/FoodPin/RestaurantListView.swift
+++ FoodPin/FoodPin/RestaurantListView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct RestaurantListView: View {
     
+    @State var restaurantIsFavorites = Array(repeating: false, count: 21)
+    
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
@@ -20,29 +22,8 @@ struct RestaurantListView: View {
     var body: some View {
         
         List {
-            
-            ForEach(restaurantNames.indices, id: \.self) { restaurantName in
-                VStack(alignment: .leading, spacing: 10) {
-                    Image(restaurantImages[restaurantName])
-                            .resizable()
-                            .scaledToFill()
-                            .frame(height: 200)
-                            .cornerRadius(25)
-                    
-                    VStack(alignment: .leading){
-                        Text(restaurantNames[restaurantName])
-                            .font(.system(.title2, design: .rounded))
-                        
-                        Text(restaurantTypes[restaurantName])
-                            .font(.system(.body, design: .rounded))
-                        
-                        Text(restaurantLocations[restaurantName])
-                            .font(.system(.subheadline, design: .rounded))
-                            .foregroundColor(.gray)
-                    }
-                    .padding(.horizontal)
-                    .padding(.bottom)
-                }
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                BasicTextImageRow(imageName: restaurantImages[index], name: restaurantNames[index], type: restaurantTypes[index], location: restaurantLocations[index], isFavorite: $restaurantIsFavorites[index])
             }
             .listRowSeparator(.hidden)
         }
@@ -50,11 +31,152 @@ struct RestaurantListView: View {
     }
 }
 
+struct BasicTextImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    @State private var showOptinons = false
+    @State private var showError = false
+    
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 20) {
+            Image(imageName)
+                .resizable()
+                .frame(width: 120, height: 118)
+                .cornerRadius(20)
+            
+            VStack(alignment: .leading) {
+                Text(name)
+                    .font(.system(.title2, design: .rounded))
+                
+                Text(type)
+                    .font(.system(.body, design: .rounded))
+                
+                Text(location)
+                    .font(.system(.subheadline, design: .rounded))
+                    .foregroundColor(.gray)
+            }
+            if isFavorite {
+                Spacer()
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.red)
+            }
+        }
+        .onTapGesture {
+            showOptinons.toggle()
+        }
+        .actionSheet(isPresented: $showOptinons) {
+            ActionSheet(title: Text("What do you want to do?"),
+                        message: nil,
+                        buttons: [
+                            .default(Text("Reserve a table")) {
+                                self.showError.toggle()
+                            },
+                            .default(Text(isFavorite ? "Remove from favorites" : "Mark as favorite")) {
+                                self.isFavorite.toggle()
+                            },
+                            .cancel()
+                        ])
+            
+        }
+        .alert(isPresented: $showError) {
+            Alert(title: Text("Not available"),
+                  message: Text("Sorry, this feature is not available yet. Please retry later."),
+                  primaryButton: .default(Text("OK")),
+                  secondaryButton: .cancel())
+        }
+    }
+}
+
+// -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+struct FullImageRow: View {
+    
+    var imageName: String
+    var name: String
+    var type: String
+    var location: String
+    
+    @State private var showOptinons = false
+    @State private var showError = false
+    
+    @Binding var isFavorite: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Image(imageName)
+                .resizable()
+                .scaledToFill()
+                .frame(height: 200)
+                .cornerRadius(20)
+            
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text(name)
+                        .font(.system(.title2, design: .rounded))
+                        
+                    Text(type)
+                        .font(.system(.body, design: .rounded))
+                    
+                    Text(location)
+                        .font(.system(.subheadline, design: .rounded))
+                        .foregroundColor(.gray)
+                }
+                if isFavorite {
+                    Spacer()
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom)
+
+            
+        }
+        .onTapGesture {
+            showOptinons.toggle()
+        }
+        .actionSheet(isPresented: $showOptinons) {
+            ActionSheet(title: Text("What do you want to do?"),
+                        message: nil,
+                        buttons: [
+                            .default(Text("Reserve a table")) {
+                                self.showError.toggle()
+                            },
+                            .default(Text(isFavorite ? "Rremove from favorites" : "Mark as favorite")) {
+                                self.isFavorite.toggle()
+                            },
+                            .cancel()
+                        ])
+            
+        }
+        .alert(isPresented: $showError) {
+            Alert(title: Text("Not available"),
+                  message: Text("Sorry, this feature is not available yet. Please retry later."),
+                  primaryButton: .default(Text("OK")),
+                  secondaryButton: .cancel())
+        }
+    }
+}
+
+
+
 struct RestaurantListView_Previews: PreviewProvider {
     static var previews: some View {
         RestaurantListView()
         
         RestaurantListView()
             .preferredColorScheme(.dark)
+        
+        BasicTextImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+                    .previewLayout(.sizeThatFits)
+
+        FullImageRow(imageName: "cafedeadend", name: "Cafe Deadend", type: "Cafe", location: "Hong Kong", isFavorite: .constant(true))
+                    .previewLayout(.sizeThatFits)
     }
 }
