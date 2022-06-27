749998e90b78f43b78183099a08f9fb83e56b0ea
sla-vak <slavak15072008@gmail.com>
2022-06-28 01:26:56 +0300

    Detail View Enhancement, Custom Fonts and Navigation Bar Customization and Chapter 12 Exercise

--- FoodPin/FoodPin/View/ActivityView.swift
+++ FoodPin/FoodPin/View/ActivityView.swift
@@ -8,16 +8,16 @@
 import SwiftUI
 
 struct ActivityView: UIViewControllerRepresentable {
-
+    
     var activityItems: [Any]
     var applicationActivities: [UIActivity]? = nil
-
+    
     func makeUIViewController(context: Context) -> some UIViewController {
         let activityController = UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
         return activityController
     }
-
+    
     func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-
+        
     }
 }
