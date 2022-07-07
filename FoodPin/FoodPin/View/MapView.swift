b4550f3120fb054ec917bbac82eace2e4e6158cc
sla-vak <slavak15072008@gmail.com>
2022-07-07 15:01:33 +0300

    chapter 14 exercise

---
+++ FoodPin/FoodPin/View/MapView.swift
@@ -0,0 +1,60 @@
+//
+//  MapView.swift
+//  FoodPin
+//
+//  Created by Вячеслав Каньшин on 29.06.2022.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var location: String = ""
+    
+    @State private var region: MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.510357, longitude: -0.116773), span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0))
+    
+    @State private var annotatedItem: AnnotatedItem = AnnotatedItem(coordinate: CLLocationCoordinate2D(latitude: 51.510357, longitude: -0.116773))
+    
+    var body: some View {
+        Map(coordinateRegion: $region, interactionModes: [], annotationItems: [annotatedItem]) { item in
+            MapMarker(coordinate: item.coordinate, tint: .red)
+        }
+        .task {
+            convertAdress(location: location)
+        }
+    }
+    
+    private func convertAdress(location: String) {
+        
+        let geoCoder = CLGeocoder()
+        
+        geoCoder.geocodeAddressString(location, completionHandler: { placemarks, error in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let placemarks = placemarks,
+                  let location = placemarks[0].location else {
+                return
+            }
+            
+            self.region = MKCoordinateRegion(center: location.coordinate, span: MKCoordinateSpan(latitudeDelta: 0.0015, longitudeDelta: 0.0015))
+            
+            self.annotatedItem = AnnotatedItem(coordinate: location.coordinate)
+        })
+    }
+    
+    struct AnnotatedItem: Identifiable {
+        let id = UUID()
+        var coordinate: CLLocationCoordinate2D
+    }
+    
+}
+
+struct MapView_Preview: PreviewProvider {
+    static var previews: some View {
+        MapView(location: "54 Frith Street London W1D 4SL United Kingdom")
+    }
+    
+}
--- FoodPin/FoodPin/View/RestaurantDetailView.swift
+++ FoodPin/FoodPin/View/RestaurantDetailView.swift
@@ -71,6 +71,18 @@ struct RestaurantDetailView: View {
                     .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
                 }
                 .padding(.horizontal)
+                
+                NavigationLink(
+                    destination:
+                        MapView(location: restaurant.location)
+                            .edgesIgnoringSafeArea(.all)
+                ) {
+                    MapView(location: restaurant.location)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .frame(height: 200)
+                        .cornerRadius(20)
+                        .padding()
+                }
             }
         }
         .navigationBarBackButtonHidden(true)
